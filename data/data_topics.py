6969f19d8b2772da73c884c7311d1e053460a9f9
mor80 <egmor80@gmail.com>
2023-12-15 02:53:00 +0300

    Nothing1

--- data/data_topics.py
+++ data/data_topics.py
@@ -4,4 +4,13 @@ from data_library.question import Question
 from data_library.topic import Topic
 from data_library.sub_topic import SubTopic
 
-topics = [Topic(f"Топик{i}", f"Описание топика{i}", [SubTopic(f"СапТопик{j}", [Question(f"Вопрос{j} {k}", "1", 3) for k in range(4)]) for j in range(4)]) for i in range(4)]
+topics_name = ["Культура СССР", "Правление Романовых", "Древняя Русь"]
+sub_topics_name = [["Фильмы", "Плакаты", "Песни", "Архитектура"],
+                   ["Персоналии", "События", "Портреты", "Разное"],
+                   ["Князья", "Даты", "Термины", "Разное"]]
+
+questions = [[[]], [[]]]
+
+
+topics = [Topic(topic, f"", [SubTopic(stn, [Question(f"Вопрос{j} {k}", "1", 3) for k in range(4)])
+                             for stn in sub_topics_name[i]]) for i, topic in enumerate(topics_name)]
