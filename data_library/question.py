dfa30fbae13a28c85f998aad2e824c5a2389b004
mor80 <egmor80@gmail.com>
2023-12-15 22:34:04 +0300

    questions

--- data_library/question.py
+++ data_library/question.py
@@ -1,8 +1,10 @@
 class Question:
-    def __init__(self, question_text, correct_answer: str, answer_count: int, information: str, video=None, audio=None):
+    def __init__(self, question_text, correct_answer: str, answer_count: int, information: str, output_photo=None, first_photo=None, video=None, audio=None):
         self.question_text = question_text
         self.correct_answer = correct_answer
         self.answer_count = answer_count
         self.information = information
+        self.first_photo = first_photo
+        self.output_photo = output_photo
         self.video = video
         self.audio = audio
