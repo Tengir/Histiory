2755cff221a03d6799af19e30c8ae663ff81c807
mor80 <egmor80@gmail.com>
2023-12-15 16:37:11 +0300

    New

--- data_library/question.py
+++ data_library/question.py
@@ -1,7 +1,8 @@
 class Question:
-    def __init__(self, question_text, correct_answer: str, answer_count: int, video=None, audio=None):
+    def __init__(self, question_text, correct_answer: str, answer_count: int, information: str, video=None, audio=None):
         self.question_text = question_text
         self.correct_answer = correct_answer
         self.answer_count = answer_count
+        self.information = information
         self.video = video
         self.audio = audio
